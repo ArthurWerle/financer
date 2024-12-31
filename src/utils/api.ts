25494a1dbd83a5b5291a17aa08a26f6adcaa8e72
arthurwerle <arthur.werle@gmail.com>
2024-12-31 09:59:47 -0300

    add base url to env file

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,9 @@
 import axios from "axios"
 
+const baseUrl = process.env.REACT_APP_BASE_URL || "http://localhost:3000"
+
 const api = axios.create({
+  baseURL: baseUrl,
   headers: {
     "Content-Type": "application/json",
   },
