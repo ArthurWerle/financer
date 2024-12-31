3fc9649215a1fc7856efbbc2d3dd80e0f7b3e0c1
arthurwerle <arthur.werle@gmail.com>
2024-12-31 10:18:30 -0300

    update baseurl

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseUrl = process.env.REACT_APP_BASE_URL || "http://localhost:3000"
+const baseUrl = process.env.REACT_APP_BASE_URL || "http://localhost"
 
 const api = axios.create({
   baseURL: baseUrl,
