4c93e7a07eade313fecdea791fd74a244d521801
arthurwerle <arthur.werle@gmail.com>
2024-12-31 11:20:44 -0300

    change way to know base url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
-export const BFF_BASE_URL = process.env.NEXT_PUBLIC_API_URL || ":8082/api/bff"
-export const CATEGORY_SERVICE_BASE_URL = process.env.NEXT_PUBLIC_CATEGORY_SERVICE_URL || ":8080/api"
-export const TRANSACTION_SERVICE_BASE_URL = process.env.NEXT_PUBLIC_TRANSACTION_SERVICE_URL || ":8081/api"
+const baseURL = process.env.NODE_ENV === 'production' ? 'http://192.168.2.125' : 'http://localhost'
+
+export const BFF_BASE_URL = `${baseURL}:8082/api/bff`
+export const CATEGORY_SERVICE_BASE_URL = `${baseURL}:8080/api`
+export const TRANSACTION_SERVICE_BASE_URL = `${baseURL}:8081/api`
