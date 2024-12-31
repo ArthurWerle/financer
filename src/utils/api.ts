83dab77c22dc2fddefc65795e6a406bc6634f5cc
arthurwerle <arthur.werle@gmail.com>
2024-12-31 10:31:41 -0300

    add request logs

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,21 +1,25 @@
 import axios from "axios"
 
-const baseUrl = process.env.REACT_APP_BASE_URL || "http://localhost"
-
 const api = axios.create({
-  baseURL: baseUrl,
   headers: {
     "Content-Type": "application/json",
   },
 })
 
-// Add a request interceptor to include the auth token
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token")
-  if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`
-  }
+api.interceptors.request.use(config => {
+  console.log('Request:', config.method?.toUpperCase(), config.url)
   return config
 })
+
+api.interceptors.response.use(
+  response => {
+    console.log('Response:', response.status, response.config.url)
+    return response
+  },
+  error => {
+    console.error('Error:', error.response?.status, error.config?.url)
+    return Promise.reject(error)
+  }
+)
 
 export default api
