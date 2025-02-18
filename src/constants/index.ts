1b18204c4877784b448a2c346fba6f9e6c321122
arthurwerle <arthur.werle@gmail.com>
2025-02-18 20:25:11 -0300

    remove log

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,5 @@
 const baseURL = process.env.NODE_ENV === 'production' ? `http://${process.env.NEXT_PUBLIC_SERVER_BASE_URL}` : 'http://localhost'
 
-console.log({
-    env: process.env
-})
-
 export const BFF_BASE_URL = `${baseURL}:8082/api/bff` as const
 export const CATEGORY_SERVICE_BASE_URL = `${baseURL}:8080/api` as const
 export const TRANSACTION_SERVICE_BASE_URL = `${baseURL}:8081/api` as const
