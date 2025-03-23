4b48355b7e4bece530e552e6d2a8e75219377ae6
arthurwerle <arthur.werle@gmail.com>
2025-03-23 20:53:56 -0300

    improve popover UX

--- src/components/add-expense.tsx
+++ src/components/add-expense.tsx
@@ -1,13 +1,29 @@
 import { useMemo, useState } from 'react'
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { Calendar } from "@/components/ui/calendar"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
-import { format } from "date-fns"
-import { CalendarIcon } from "lucide-react"
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { format } from 'date-fns'
+import { CalendarIcon } from 'lucide-react'
 import { useCategories } from '../queries/categories/useCategories'
 import { useTypes } from '../queries/types/useTypes'
 import { addTransaction } from '../queries/transactions/addTransaction'
@@ -23,14 +39,19 @@ import { addDate } from '../utils/add-date'
 
 type FormData = Partial<
   Omit<Transaction, 'id' | 'typeName' | 'createdAt' | 'updatedAt'> &
-  Omit<RecurringTransaction, 'id' | 'typeName' | 'createdAt' | 'updatedAt' | 'startDate'>> & {
-    date: Date
-    startDate: Date
-    installments: number | undefined
-  }
+    Omit<
+      RecurringTransaction,
+      'id' | 'typeName' | 'createdAt' | 'updatedAt' | 'startDate'
+    >
+> & {
+  date: Date
+  startDate: Date
+  installments: number | undefined
+}
 
 export const AddExpense = () => {
   const today = new Date()
+  const [datePickerOpen, setDatePickerOpen] = useState(false)
 
   const [formData, setFormData] = useState<FormData>({
     amount: undefined,
@@ -50,12 +71,12 @@ export const AddExpense = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false)
   const { data: categories, isLoading: isLoadingCategories } = useCategories()
   const { data: types, isLoading: isLoadingTypes } = useTypes()
-  const expenseTypeId = types?.find(type => type.Name === 'expense')?.ID
+  const expenseTypeId = types?.find((type) => type.Name === 'expense')?.ID
 
   const formValidation = useMemo(() => {
     let validation = {
       isValid: true,
-      invalidFields: [] as string[]
+      invalidFields: [] as string[],
     }
 
     if (!formData.amount || !formData.categoryId || !formData.description) {
@@ -67,27 +88,27 @@ export const AddExpense = () => {
       if (!formData.amount) {
         validation = {
           ...validation,
-          invalidFields: ['Amount']
+          invalidFields: ['Amount'],
         }
       }
 
       if (!formData.categoryId) {
         validation = {
           ...validation,
-          invalidFields: [...validation.invalidFields, 'Category']
+          invalidFields: [...validation.invalidFields, 'Category'],
         }
       }
-      
+
       if (!formData.description) {
         validation = {
           ...validation,
-          invalidFields: [...validation.invalidFields, 'Description']
+          invalidFields: [...validation.invalidFields, 'Description'],
         }
       }
     }
 
     return validation
-  }, [formData.amount, formData.categoryId, formData.description]) 
+  }, [formData.amount, formData.categoryId, formData.description])
 
   const handleSubmit = async (e: any) => {
     setIsLoading(true)
@@ -96,28 +117,37 @@ export const AddExpense = () => {
     const formDataWithTypeIdAndEndDate = {
       ...formData,
       typeId: expenseTypeId,
-      endDate: formData.installments ? addDate(formData.date, formData.installments) : undefined
+      endDate: formData.installments
+        ? addDate(formData.date, formData.installments)
+        : undefined,
     }
 
     await addTransaction(formDataWithTypeIdAndEndDate)
       .catch((error) => {
         console.log({ error })
-        toast.error(`ERROR: ${error?.message || 'Error creating transaction'}`, {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: false,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "colored",
-        })
+        toast.error(
+          `ERROR: ${error?.message || 'Error creating transaction'}`,
+          {
+            position: 'top-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'colored',
+          }
+        )
       })
       .finally(() => {
         queryClient.invalidateQueries({ queryKey: MONTH_OVERVIEW_QUERY_KEY })
-        queryClient.invalidateQueries({ queryKey: EXPENSE_COMPARSION_HISTORY_QUERY_KEY })
+        queryClient.invalidateQueries({
+          queryKey: EXPENSE_COMPARSION_HISTORY_QUERY_KEY,
+        })
         queryClient.invalidateQueries({ queryKey: TRANSACTIONS_QUERY_KEY })
-        queryClient.invalidateQueries({ queryKey: CATEGORIES_MONTHLY_EXPENSE_QUERY_KEY })
+        queryClient.invalidateQueries({
+          queryKey: CATEGORIES_MONTHLY_EXPENSE_QUERY_KEY,
+        })
         setIsLoading(false)
         setIsDialogOpen(false)
       })
@@ -126,7 +156,13 @@ export const AddExpense = () => {
   return (
     <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
       <DialogTrigger asChild>
-        <Button variant="default" id="add-expense-button" onClick={() => setIsDialogOpen(true)}>New Expense</Button>
+        <Button
+          variant="default"
+          id="add-expense-button"
+          onClick={() => setIsDialogOpen(true)}
+        >
+          New Expense
+        </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
@@ -140,15 +176,19 @@ export const AddExpense = () => {
                 id="amount"
                 type="number"
                 value={formData.amount}
-                onChange={(e) => setFormData({ ...formData, amount: Number(e.target.value) })}
+                onChange={(e) =>
+                  setFormData({ ...formData, amount: Number(e.target.value) })
+                }
                 required
               />
             </div>
-            
+
             <div className="grid gap-2">
               <Label htmlFor="category">Category</Label>
-              <Select 
-                onValueChange={(value) => setFormData({ ...formData, categoryId: Number(value) })} 
+              <Select
+                onValueChange={(value) =>
+                  setFormData({ ...formData, categoryId: Number(value) })
+                }
                 required
               >
                 <SelectTrigger>
@@ -156,7 +196,9 @@ export const AddExpense = () => {
                 </SelectTrigger>
                 <SelectContent>
                   {isLoadingCategories ? (
-                    <SelectItem value="" disabled>Loading categories...</SelectItem>
+                    <SelectItem value="" disabled>
+                      Loading categories...
+                    </SelectItem>
                   ) : (
                     categories?.map((category) => (
                       <SelectItem key={category.ID} value={String(category.ID)}>
@@ -170,18 +212,26 @@ export const AddExpense = () => {
 
             <div className="grid gap-2">
               <Label>Date</Label>
-              <Popover>
+              <Popover open={datePickerOpen} onOpenChange={setDatePickerOpen}>
                 <PopoverTrigger asChild>
-                  <Button variant="outline" className="justify-start text-left font-normal">
+                  <Button
+                    variant="outline"
+                    className="justify-start text-left font-normal"
+                  >
                     <CalendarIcon className="mr-2 h-4 w-4" />
-                    {formData.date ? format(formData.date, "PPP") : "Pick a date"}
+                    {formData.date
+                      ? format(formData.date, 'PPP')
+                      : 'Pick a date'}
                   </Button>
                 </PopoverTrigger>
                 <PopoverContent className="w-auto p-0">
                   <Calendar
                     mode="single"
                     selected={formData.date}
-                    onSelect={(date) => setFormData({ ...formData, date })}
+                    onSelect={(date) => {
+                      setFormData({ ...formData, date })
+                      setDatePickerOpen(false)
+                    }}
                     initialFocus
                   />
                 </PopoverContent>
@@ -193,14 +243,20 @@ export const AddExpense = () => {
               <Input
                 id="description"
                 value={formData.description}
-                onChange={(e) => setFormData({ ...formData, description: e.target.value })}
+                onChange={(e) =>
+                  setFormData({ ...formData, description: e.target.value })
+                }
                 required
               />
             </div>
 
             <div className="grid gap-2">
               <Label htmlFor="frequency">Frequency</Label>
-              <Select onValueChange={(value) => setFormData({ ...formData, frequency: value })}>
+              <Select
+                onValueChange={(value) =>
+                  setFormData({ ...formData, frequency: value })
+                }
+              >
                 <SelectTrigger>
                   <SelectValue placeholder="Select frequency" />
                 </SelectTrigger>
@@ -211,7 +267,7 @@ export const AddExpense = () => {
                   <SelectItem value="yearly">Yearly</SelectItem>
                 </SelectContent>
               </Select>
-            </div> 
+            </div>
 
             {formData.frequency && (
               <div className="grid gap-2">
@@ -220,19 +276,37 @@ export const AddExpense = () => {
                   id="installments"
                   type="number"
                   value={formData.installments}
-                  onChange={(e) => setFormData({ ...formData, installments: Number(e.target.value) })}
+                  onChange={(e) =>
+                    setFormData({
+                      ...formData,
+                      installments: Number(e.target.value),
+                    })
+                  }
                 />
               </div>
             )}
           </div>
-          
-          <Button type="submit" className="w-full" disabled={isLoading || isLoadingTypes || !expenseTypeId || !formValidation.isValid }>
-            {isLoading || isLoadingTypes || !expenseTypeId ? "Loading..." : "Create"}
+
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={
+              isLoading ||
+              isLoadingTypes ||
+              !expenseTypeId ||
+              !formValidation.isValid
+            }
+          >
+            {isLoading || isLoadingTypes || !expenseTypeId
+              ? 'Loading...'
+              : 'Create'}
           </Button>
           {!formValidation.isValid && (
             <ul>
-              {formValidation.invalidFields.map(invalidField => (
-                <li key={invalidField} className='text-red-500 text-[11px]'>Field &quot;{invalidField}&quot; is missing</li>
+              {formValidation.invalidFields.map((invalidField) => (
+                <li key={invalidField} className="text-red-500 text-[11px]">
+                  Field &quot;{invalidField}&quot; is missing
+                </li>
               ))}
             </ul>
           )}
