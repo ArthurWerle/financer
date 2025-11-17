90477a9be4e92e87b749c85262a59fc52c9ac0c1
arthurwerle <arthur.werle@gmail.com>
2025-11-17 12:50:38 -0300

    call BFF instead of service directly

--- src/queries/transactions/addTransaction.ts
+++ src/queries/transactions/addTransaction.ts
@@ -1,19 +1,22 @@
-import { TRANSACTION_SERVICE_BASE_URL } from "@/constants"
-import { RecurringTransaction } from "@/types/recurring-transaction"
-import { Transaction } from "@/types/transaction"
-import api from "@/utils/api"
-
+import { BFF_BASE_URL } from '@/constants'
+import { RecurringTransaction } from '@/types/recurring-transaction'
+import { Transaction } from '@/types/transaction'
+import api from '@/utils/api'
 
 type PostTransactionType = Partial<
   Omit<Transaction, 'id' | 'typeName' | 'createdAt' | 'updatedAt'> &
-  Omit<RecurringTransaction, 'id' | 'typeName' | 'createdAt' | 'updatedAt' | 'lastOccurrence'>>
+    Omit<
+      RecurringTransaction,
+      'id' | 'typeName' | 'createdAt' | 'updatedAt' | 'lastOccurrence'
+    >
+>
 
 export const addTransaction = async (transaction: PostTransactionType) => {
   const isRecurring = transaction?.frequency?.length ?? 0 > 0
 
   if (isRecurring) {
-    return await api.post(`${TRANSACTION_SERVICE_BASE_URL}/recurring-transactions`, transaction)
+    return await api.post(`${BFF_BASE_URL}/recurring-transactions`, transaction)
   }
 
-  return await api.post(`${TRANSACTION_SERVICE_BASE_URL}/transactions`, transaction)
+  return await api.post(`${BFF_BASE_URL}/transactions`, transaction)
 }
